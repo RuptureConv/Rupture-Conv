aee03550053f29ce8e3f1d731dd8e3b8d0fafdad
Vincent GIRBAL <vgirbal@gmail.com>
2026-05-05 14:26:00 +0200

    Add Google Search Console head verification

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="m9cDeMi4TyzceUF4V6KsuQcYqaG95ObYZwsm5OrQnd8"
+      />
+    </>
+  );
+}
